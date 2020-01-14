982f8e8a72bc7676fa64f7bb624c0d3e5fa96169
zdd <zdd2389@gmail.com>
2020-01-14 17:05:24 +0800

    '所有代码变量的配置都应该在productFlavors中设置'

---
+++ app/src/main/java/com/zdd/gradledemo/Constant.java
@@ -0,0 +1,9 @@
+package com.zdd.gradledemo;
+
+/**
+ * Created by zdd on 2020/1/14
+ */
+public class Constant {
+    public static final String SERVE_URL=BuildConfig.API_HOST;
+    public static final boolean DEBUG=BuildConfig.LOG_DEBUG;
+}
